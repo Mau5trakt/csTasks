cf7ee5224d1db197ca7affa755ab29eaeb773f81
Mau5trakt <kubzalemany@hotmail.com>
2024-08-05 23:52:58 -0600

    First Commit

---
+++ tasks/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Agregar Tarea", widget=forms.TextInput(attrs={'placeholder': "Nueva Tarea", "is_required": True}))
