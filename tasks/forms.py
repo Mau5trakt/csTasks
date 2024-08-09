d048bb706d3d1c74a3d303d10021c157d747cd77
Mau5trakt <kubzalemany@hotmail.com>
2024-08-08 23:43:03 -0600

    refactor and added class

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
+
 class NewTaskForm(forms.Form):
-    task = forms.CharField(label="Agregar Tarea", widget=forms.TextInput(attrs={'placeholder': "Nueva Tarea", "is_required": True}))
+    task = forms.CharField(
+        label="Agregar Tarea",
+        widget=forms.TextInput(
+            attrs={'placeholder': "Nueva Tarea", "is_required": True, "class": "form-control form-control-lg"}
+        )
+    )
