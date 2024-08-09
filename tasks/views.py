c59c55b5e112721f35cf81ce1db6f04972810347
Mau5trakt <kubzalemany@hotmail.com>
2024-08-08 23:44:26 -0600

    create deleting task logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, reverse, redirect
+from django.views.decorators.http import require_POST
+
 from .forms import NewTaskForm
 # Create your views here.
 tasks_list = ["Primer tarea", "Segunda tarea", "Tercer tarea"]
@@ -29,4 +31,12 @@ def add(request):
     return render(request, 'tasks/add.html', {"form": NewTaskForm})
 
 
+def show_to_delete(request):
 
+    return render(request, 'tasks/delete.html', {"tasks_list": tasks_list})
+@require_POST
+def delete(request):
+    print(request.POST)
+    task = request.POST['task']
+    tasks_list.remove(task)
+    return redirect(reverse('tasks:index'))
